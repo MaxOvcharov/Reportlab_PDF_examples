d2151781375ec0e797cd2d6a1198de5866abde6e
MaxOvcharov <ovcharovmax@yandex.ru>
2017-09-06 23:07:10 +0300

    Added JSON data example

---
+++ json_data.py
@@ -0,0 +1,195 @@
+JSON_DATA_EXAMPLE = {
+    "EventID": 658536,
+    "IsMain": True,
+    "Message": None,
+    "OutServiceID": 34161,
+    "State": 0,
+    "Vmid": "10.11.32.220",
+    "CommonInfo": {
+        "CategoryTC": "B",
+        "Color": "СИНИЙ",
+        "CorpusTip": "ЛЕГКОВОЙ",
+        "EcologClass": "нет данных",
+        "KWT": "140",
+        "KolVl": "4",
+        "LS": "190",
+        "Model": "ХОНДА АККОРД",
+        "PermissWeight": "1930",
+        "StatevyvozName": "",
+        "V3": "2354",
+        "Year": "2004"
+    },
+    "DTP_Part": {
+        "DTP": [
+            {
+                "DTPDamage": "перед справа, справа, перед слева",
+                "DTPDate": "10.01.2011",
+                "DTPRegionKod": "г. Москва",
+                "DTPRowNum": "1",
+                "DTPType": "Столкновение"
+            },
+            {
+                "DTPDamage": "",
+                "DTPDate": "07.07.2007",
+                "DTPRegionKod": "Московская область",
+                "DTPRowNum": "2",
+                "DTPType": "Наезд на пешехода"
+            },
+            {
+                "DTPDamage": "",
+                "DTPDate": "15.10.2005",
+                "DTPRegionKod": "г. Москва",
+                "DTPRowNum": "3",
+                "DTPType": "Опрокидывание"
+            },
+            {
+                "DTPDamage": "",
+                "DTPDate": "15.10.2005",
+                "DTPRegionKod": "г. Москва",
+                "DTPRowNum": "4",
+                "DTPType": "Опрокидывание"
+            }
+        ],
+        "DTPComment": "Отображаются сведения о дорожно-транспортных происшествиях, в которых участвовало транспортное средство, независимо от степени вины водителя. Вид ДТП указывается одинаковый для каждого участника ДТП."
+    },
+    "GTO_Part": {
+        "GTO": [
+            {
+                "GTODate": "05.03.2016",
+                "GTORegionKod": "Московская область/Какая то компания 2",
+                "GTORes": "ТС неисправно",
+                "GTORowNum": "1"
+            },
+            {
+                "GTODate": "05.03.2016",
+                "GTORegionKod": "Московская область/Какая то компания 2",
+                "GTORes": "ТС неисправно",
+                "GTORowNum": "2"
+            },
+            {
+                "GTODate": "01.03.2015",
+                "GTORegionKod": "Московская область/Какая то компания 2",
+                "GTORes": "ТС неисправно",
+                "GTORowNum": "3"
+            },
+            {
+                "GTODate": "01.03.2014",
+                "GTORegionKod": "г. Москва/Какая то компания",
+                "GTORes": "ТС исправно",
+                "GTORowNum": "4"
+            }
+        ],
+        "GTOComment": "Информация предоставляется о результатах технического осмотра, проведенного до 01.01.2013"
+    },
+    "Insurance_Part": {
+        "Insurance": [
+            {
+                "InsuranceAccident": "Дорожно-транспортное происшествие",
+                "InsuranceDamage": "Передний бампер; Капот",
+                "InsuranceDate": "27.11.2015",
+                "InsuranceType": "ОСАГО"
+            }
+        ],
+        "InsuranceComment": "Отображаются сведения о повреждениях, зафиксированных при обращении в страховые компании"
+    },
+    "Kommercial_Part": {
+        "Kommercial": [
+            {
+                "KommercialLicenseStop": "нет",
+                "KommercialStartDate": "Использование в качестве такси"
+            },
+            {
+                "KommercialLicenseStop": "нет",
+                "KommercialStartDate": "Использование в качестве маршрутного транспорта"
+            },
+            {
+                "KommercialLicenseStop": "нет",
+                "KommercialStartDate": "Использование в качестве грузового транспорта"
+            },
+            {
+                "KommercialLicenseStop": "нет",
+                "KommercialStartDate": "Использование в качестве специального транспорта (городские службы, аварийные службы и прочее)"
+            },
+            {
+                "KommercialLicenseStop": "нет",
+                "KommercialStartDate": "Прочие виды"
+            }
+        ]
+    },
+    "Mileage_Part": {
+        "Mileage": [
+            {
+                "MileageDate": "27.11.2015",
+                "MileageVal": "275000"
+            }
+        ],
+        "MileageComment": ""
+    },
+    "Mortgage": {},
+    "Options": {},
+    "Restrictions_Part": {
+        "Restrictions": [
+            {
+                "OgrKod": "Запрет на снятие с учета",
+                "OgrName": "Запрет на снятие с учета",
+                "OgrRes": "нет"
+            },
+            {
+                "OgrKod": "Запрет на регистрационные действия и прохождение ТО",
+                "OgrName": "Запрет на регистрационные действия и прохождение ТО",
+                "OgrRes": "нет"
+            },
+            {
+                "OgrKod": "Утилизация (для транспорта не старше 5 лет)",
+                "OgrName": "Утилизация (для транспорта не старше 5 лет)",
+                "OgrRes": "нет"
+            },
+            {
+                "OgrKod": "Аннулирование регистрации",
+                "OgrName": "Аннулирование регистрации",
+                "OgrRes": "нет"
+            },
+            {
+                "OgrKod": "Нахождение в розыске",
+                "OgrName": "Нахождение в розыске",
+                "OgrRes": "нет"
+            }
+        ],
+        "RestrictionsComment": "Предоставляется информация о наличии сведений о запретах или ограничениях по изменению права собственности на транспортное средство, наложенных судами, следственными и таможенными органами, органами социальной защиты населения либо другими органами в случаях и порядке, предусмотренных законодательством Российской Федерации."
+    },
+    "Services": {},
+    "TrucksRestrictions_Part": None,
+    "VladHist": {
+        "VladHistComment": None,
+        "VladHistTable": [
+            {
+                "VladPeriod": "окт 2004 - май 2005",
+                "VladPeriodEnd": "10.05.2005",
+                "VladPeriodStart": "10.10.2004",
+                "VladRowNum": "Владелец 1",
+                "VladType": "Юридическое лицо"
+            },
+            {
+                "VladPeriod": "май 2005 - сен 2009",
+                "VladPeriodEnd": "05.09.2009",
+                "VladPeriodStart": "12.05.2005",
+                "VladRowNum": "Владелец 2",
+                "VladType": "Физическое лицо"
+            },
+            {
+                "VladPeriod": "окт 2009 - дек 2012",
+                "VladPeriodEnd": "30.12.2012",
+                "VladPeriodStart": "01.10.2009",
+                "VladRowNum": "Владелец 3",
+                "VladType": "Физическое лицо"
+            },
+            {
+                "VladPeriod": "янв 2013 - по настоящее время",
+                "VladPeriodEnd": "",
+                "VladPeriodStart": "02.01.2013",
+                "VladRowNum": "Владелец 4",
+                "VladType": "Физическое лицо"
+            }
+        ]
+    }
+}
